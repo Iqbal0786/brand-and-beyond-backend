ab700f29f775850c546d1132ae439f03a60ca5de
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-19 21:37:09 +0530

    added user schema and   user controller

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express= require("express")
 const app= express();
 const connect= require("./configs/db");
 const cors= require("cors");
+const userController= require("./controllers/user.controller")
 
 app.use(express.json());
 app.use(cors());
-
+app.use("/users",userController)
 
 app.listen(7777, async()=>{
      try {
