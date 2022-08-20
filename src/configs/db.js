73822a77acbcb4438fc21971ac9b238684660c17
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-20 11:51:45 +0530

    added url in env

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,6 @@
+require("dotenv").config()
 const mongoose= require("mongoose");
 
 module.exports= ()=>{
-    return mongoose.connect("mongodb+srv://iqbal:iqbal_786@cluster0.daqxb.mongodb.net/BrandAndBeyond?retryWrites=true&w=majority")
+    return mongoose.connect(process.env.MONGO_URL);
 }
